95afca4dd704104469d26e04d38f6ffd479dcdc0
Emir Maya <emirmayaok@gmail.com>
2026-09-10 19:39:45 -0300

    logo added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer z-10 border-t border-yellow-50 border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-center flex-col items-center">
-        <Image className="rounded-xl my-2" src={"/logo2.svg"} alt="logo" width={100} height={100} />
+        <Image className="rounded-xl my-2" src={"/images/logo.png"} alt="logo" width={100} height={100} />
         <p className="text-[#848666] text-center leading-tight text-sm">© All rights reserved 2025</p>
       </div>
     </footer>
